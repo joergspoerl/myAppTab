fcf5e87c4cb6a461e4792b08ba6d53ecddd6cfb8
Joerg Spoerl <joerg.spoerl@gmx.de>
2017-10-09 22:16:34 +0200

    built wetter home screen

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { List } from 'ionic-angular';
+// import { MyDataService } from '../../services/my-data.service'
 
 @Component({
   selector: 'page-about',
@@ -7,8 +9,10 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+) {
+  //    public myDataService: MyDataService
   }
 
 }
