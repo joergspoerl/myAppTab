9c747f1dea82fa005c013df4acb2e75ee2c507a6
Joerg Spoerl <joerg.spoerl@gmx.de>
2017-10-20 08:17:40 +0200

    add send mail to build script

--- publish-uberspace.sh
+++ publish-uberspace.sh
@@ -1,2 +1,3 @@
 ionic cordova build android
 scp platforms/android/build/outputs/apk/android-armv7-debug.apk jrg@deneb.uberspace.de:/home/jrg/html/mw/android-armv7-debug.apk
+~/scripts/sendMail-apk-ready.sh
