132c074a2783a8c0fcb4b224c31b453832d383a6
Joerg Spoerl <joerg.spoerl@gmx.de>
2017-10-20 08:18:53 +0200

    add git push to build script

--- publish-uberspace.sh
+++ publish-uberspace.sh
@@ -1,3 +1,4 @@
+git push
 ionic cordova build android
 scp platforms/android/build/outputs/apk/android-armv7-debug.apk jrg@deneb.uberspace.de:/home/jrg/html/mw/android-armv7-debug.apk
 ~/scripts/sendMail-apk-ready.sh
