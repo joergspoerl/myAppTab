9d92b4ded8d30af3b52b07d8631d7e5a9ae7c9b8
Joerg Spoerl <joerg.spoerl@gmx.de>
2017-10-22 06:03:52 +0200

    fix about page

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-//import { List } from 'ionic-angular';
-// import { MyDataService } from '../../services/my-data.service'
 import { Http, Headers, RequestOptions } from '@angular/http'; // Http - angular < 4.3
 
 declare global {
@@ -29,45 +27,14 @@ export class AboutPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad AboutPage window', window);
 
-    this.initCouchDB();
-    //this.useCouchbase();
   }
 
-
-  initCouchDB() {
-    function log(status) { console.log(status); }
-
-    var couchbaseUrl;
-
-    log("Start Couchbase Init")
-    console.log("Start Couchbase Init")
-    if (window.cblite) {
-      window.cblite.getURL(function (err, url) {
-        if (err) {
-          console.log("error launching Couchbase Lite: " + err);
-          log("error launching Couchbase Lite: " + err)
-        } else {
-          console.log("Couchbase Lite running at " + url);
-          log("Couchbase Lite running at " + url);
-          couchbaseUrl = url;
-        }
-      });
-    } else {
-      console.log("error, Couchbase Lite plugin not found.")
-      log("error, Couchbase Lite plugin not found.")
-    }
-
-    //this.couchbaseUrl = couchbaseUrl;
-
-    console.log("end init this.couchbaseUrl", this.couchbaseUrl);
-
-  }
 
   dummy () {
-    console.log("dummy start -->", this.couchbaseUrl);
+    console.log("dummy start -->", this.testUrl);
 
     let options = new RequestOptions({ withCredentials: true });
-    this.http.get(this.couchbaseUrl, options).subscribe(
+    this.http.get(this.testUrl, options).subscribe(
       
       result => {
         console.log("result: ", JSON.stringify(result))
