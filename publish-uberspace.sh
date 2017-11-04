ac5a3eaf6f4a6b9b9df2796adf68bfd84b64857d
Joerg Spoerl <joerg.spoerl@gmx.de>
2017-11-04 12:29:41 +0100

    user path for sendMail-apk-read.sh

--- publish-uberspace.sh
+++ publish-uberspace.sh
@@ -1,6 +1,6 @@
 git push
 ionic cordova build android
 scp platforms/android/build/outputs/apk/android-armv7-debug.apk jrg@deneb.uberspace.de:/home/jrg/html/mw/android-armv7-debug.apk
-~/scripts/sendMail-apk-ready.sh
+~/scripts/sendMail-apk-ready.sh "http://jrg.deneb.uberspace.de/mw/android-armv7-debug.apk"
 
 # add manual in config.xml -> <preference name="loadUrlTimeoutValue" value="700000" />
